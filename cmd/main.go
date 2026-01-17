e499f734d769b1ca47717d24ac95d1e4fa67ca67
mvidenma <videnmagermarios@gmail.com>
2026-01-17 14:57:05 +0200

    Fixed path for html and css files in main

--- cmd/main.go
+++ cmd/main.go
@@ -16,7 +16,7 @@ func main() {
 
 	//Root path Parsing to the path of the mainpage.html and sending data via template
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, _ := template.ParseFiles("internal/template/mainpage.html")
+		tmpl, _ := template.ParseFiles("mainpage.html")
 		data := struct {
 			Welcome string
 		}{
@@ -27,7 +27,7 @@ func main() {
 	//Loads the style.css file
 	http.HandleFunc("/style.css", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/css")
-		http.ServeFile(w, r, "internal/template/style.css")
+		http.ServeFile(w, r, "style.css")
 	})
 	//Starts the server on port 8080
 	http.ListenAndServe(":8080", nil)
